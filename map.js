50300b3d8e431f6ed8d74d601155a0a52fc339e7
alscotty <alawrencescott@gmail.com>
2020-01-27 14:56:15 -0800

    1st map

---
+++ map.js
@@ -0,0 +1,12 @@
+
+
+let myMap = L.map('map-id').setView([37.773972, -122.431297], 12);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    accessToken: 'pk.eyJ1IjoiYWxzY290dHkiLCJhIjoiY2s0a3V2M3g1MmE4bTNubXc2eWYyb3c0aCJ9.lp7V9SIcH32Y4jgXH7VWUw'
+}).addTo(myMap);
+
+L.geoJSON(boundaries).addTo(myMap)
